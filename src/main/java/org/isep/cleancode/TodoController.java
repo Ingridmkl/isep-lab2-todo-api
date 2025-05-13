ed098235f89309ebbaa899fdd4d64357fe7b723a
Ingridmkl <ingrid.moukala@eleve.isep.fr>
2025-05-13 21:22:29 +0200

    Add gson builder adaptator due to API server failure for step 1

--- src/main/java/org/isep/cleancode/TodoController.java
+++ src/main/java/org/isep/cleancode/TodoController.java
@@ -1,9 +1,10 @@
 package org.isep.cleancode;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import spark.Request;
 import spark.Response;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +12,23 @@ public class TodoController {
 
     // this Todo class should be completed to achieve Step 1
 
-    private static final Gson gson = new Gson();
+    // private static final Gson gson = new Gson();
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
+                @Override
+                public LocalDate deserialize(JsonElement json, java.lang.reflect.Type typeOfT,
+                                             JsonDeserializationContext context) throws JsonParseException {
+                    return LocalDate.parse(json.getAsString());
+                }
+            })
+            .registerTypeAdapter(LocalDate.class, new JsonSerializer<LocalDate>() {
+                @Override
+                public JsonElement serialize(LocalDate src, java.lang.reflect.Type typeOfSrc,
+                                             JsonSerializationContext context) {
+                    return new JsonPrimitive(src.toString()); // Format: "yyyy-MM-dd"
+                }
+            })
+            .create();
     private final List<Todo> todos = new ArrayList<>();
 
     public Object getAllTodos(Request req, Response res) {
